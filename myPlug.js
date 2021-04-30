b8ff92fa10940fe43f3e8f47d5c3fb5639ca6dd7
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-30 17:08:45 +0200

    consegna

--- myPlug.js
+++ myPlug.js
@@ -2,10 +2,51 @@ const app = new Vue({
 
     el: `#vueTarget`,
     data: {
-        
+
+        immagini: [
+            { file: 'img/img1.jpg' },
+            { file: 'img/img2.jpg' },
+            { file: 'img/img3.jpg' },
+            { file: 'img/img4.jpeg' },
+            { file: 'img/img5.jpg' },
+            { file: 'img/img6.jpeg' }
+
+        ],
+
+        defaultVueImg: 2
     },
     methods :{
 
+       rightClick: function (){
+           this.defaultVueImg += 1
+
+           let limite = (this.immagini.length -1)
+
+           if(this.defaultVueImg > limite){
+            this.defaultVueImg = 0
+ 
+           }
+           
+        },
+           
+       
+
+       leftClick: function (){
+            this.defaultVueImg += -1
+            
+
+            let limite = 0
+
+            if(this.defaultVueImg < limite ){
+               this.defaultVueImg = (this.immagini.length - 1)
+            }
+            
+       },
+
+       dotClick: function (myPicIndex){
+           this.defaultVueImg = myPicIndex 
+       }
+    
     }
 
 
